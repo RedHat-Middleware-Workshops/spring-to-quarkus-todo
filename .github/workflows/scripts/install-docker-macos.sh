a8efa5d9e146db719096fee288d887fbd355eb21
Eric Deandrea <eric.deandrea@gmail.com>
2021-09-28 10:48:47 -0400

    One last check

--- .github/workflows/scripts/install-docker-macos.sh
+++ .github/workflows/scripts/install-docker-macos.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-mkdir -p ~/.docker/machine/cache
-curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
-brew uninstall virtualbox
-brew update
-brew install --cask virtualbox
-brew install docker docker-machine
-docker-machine create --driver virtualbox default
-docker-machine env default
+# Taken from https://github.com/actions/virtual-environments/issues/1143#issuecomment-652264388
+
+# mkdir -p ~/.docker/machine/cache
+# curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
+# brew uninstall virtualbox
+# brew update
+# brew install --cask virtualbox
+# brew install docker docker-machine
+# docker-machine create --driver virtualbox default
+# docker-machine env default
+
+brew cask install docker
+# allow the app to run without confirmation
+xattr -d -r com.apple.quarantine /Applications/Docker.app
+
+# preemptively do docker.app's setup to avoid any gui prompts
+sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
+sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
+sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+open -g -a Docker.app
+
+# Wait for the server to start up, if applicable.
+i=0
+while ! docker system info &>/dev/null; do
+  (( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
+  sleep 1
+done
+(( i )) && printf '\n'
+
+echo "-- Docker is ready."
