76e4f0de2e80496c4dcd0f1e9d98dfb575d325a8
Eric Deandrea <eric.deandrea@gmail.com>
2021-09-28 12:10:54 -0400

    podman

--- .github/workflows/scripts/install-podman-macos.sh
+++ .github/workflows/scripts/install-podman-macos.sh
@@ -3,3 +3,5 @@
 brew install podman
 podman machine init
 podman machine start
+
+ssh-add -l                                                                                                                    [10:03:13]
