14c93bbe8a69e2393b048ed44e7360952efc2e24
Eric Deandrea <eric.deandrea@gmail.com>
2021-09-28 11:05:23 -0400

    One last try for macos

--- .github/workflows/scripts/install-docker-macos.sh
+++ .github/workflows/scripts/install-docker-macos.sh
@@ -11,24 +11,33 @@
 # docker-machine create --driver virtualbox default
 # docker-machine env default
 
-brew cask install docker
-# allow the app to run without confirmation
-xattr -d -r com.apple.quarantine /Applications/Docker.app
+# brew cask install docker
+# # allow the app to run without confirmation
+# xattr -d -r com.apple.quarantine /Applications/Docker.app
 
-# preemptively do docker.app's setup to avoid any gui prompts
-sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
-sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
-sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
-open -g -a Docker.app
+# # preemptively do docker.app's setup to avoid any gui prompts
+# sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
+# sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+# sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
+# sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
+# sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+# open -g -a Docker.app
 
-# Wait for the server to start up, if applicable.
-i=0
-while ! docker system info &>/dev/null; do
-  (( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
-  sleep 1
-done
-(( i )) && printf '\n'
+# # Wait for the server to start up, if applicable.
+# i=0
+# while ! docker system info &>/dev/null; do
+#   (( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
+#   sleep 1
+# done
+# (( i )) && printf '\n'
 
-echo "-- Docker is ready."
+# echo "-- Docker is ready."
+
+brew update
+brew cleanup
+brew install docker docker-machine docker-machine-driver-xhyve
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+# brew install --cask virtualbox
+docker-machine create --driver xhyve default
+docker-machine env default
