4ec370cb1d402955d79b42e9f2fad45457c55cc6
Eric Deandrea <eric.deandrea@gmail.com>
2021-09-28 09:40:52 -0400

    Updating assertion

--- .github/workflows/scripts/assertMta.sh
+++ .github/workflows/scripts/assertMta.sh
@@ -2,4 +2,8 @@
 
 MTA_REPORT_DIR=$GITHUB_WORKSPACE/mta-report
 
+echo "Checking whether $MTA_REPORT_DIR exists"
 [ ! -d $MTA_REPORT_DIR ] && echo "Directory $MTA_REPORT_DIR DOES NOT exist" && exit -1
+
+echo "Checking whether $MTA_REPORT_DIR/index.html exists"
+[ ! -s "$MTA_REPORT_DIR/index.html" ] && echo "File $MTA_REPORT_DIR/index.html DOES NOT exist" && exit -1
