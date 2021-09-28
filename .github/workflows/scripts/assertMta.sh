3089decd5ecf5fcc175f80775fc33cf0fb8d2dfd
Eric Deandrea <eric.deandrea@gmail.com>
2021-09-28 09:51:13 -0400

    Adding verifications

--- .github/workflows/scripts/assertMta.sh
+++ .github/workflows/scripts/assertMta.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-MTA_REPORT_DIR=$GITHUB_WORKSPACE/mta-report
+ls -l $GITHUB_WORKSPACE
 
-echo "Current dir = $(pwd)"
-echo "Workspace dir = $GITHUB_WORKSPACE"
-echo "ls of current dir:"
-echo $(ls -al)A
-echo ""
+MTA_REPORT_DIR=$GITHUB_WORKSPACE/mta-report
+MTA_REPORT_FILE=$MTA_REPORT_DIR/index.html
 
 echo "Checking whether $MTA_REPORT_DIR exists"
 [ ! -d $MTA_REPORT_DIR ] && echo "Directory $MTA_REPORT_DIR DOES NOT exist" && exit -1
 
-echo "Checking whether $MTA_REPORT_DIR/index.html exists"
-[ ! -s "$MTA_REPORT_DIR/index.html" ] && echo "File $MTA_REPORT_DIR/index.html DOES NOT exist" && exit -1
+echo "Checking whether $MTA_REPORT_FILE exists"
+[ ! -s $MTA_REPORT_FILE ] && echo "File $MTA_REPORT_FILE DOES NOT exist" && exit -1
+
+ls -l $MTA_REPORT_DIR
+cat $MTA_REPORT_FILE
 
 exit 0
